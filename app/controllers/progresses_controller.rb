d40785fe045c0f494bc6151b0c6b782d80e5ac37
Zuzana Dostalova <dostalovaz@gmail.com>
2015-04-04 17:09:33 +0200

    Creating new note

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -2,6 +2,11 @@ class ProgressesController < AuthenticatedController
   def create
     # TODO - show failed validations
     progress = Progress.create!(progress_params)
+
+    if !params[:note].blank?
+      Note.create(text: params[:note], reading_status: progress.reading_status)
+    end
+
     flash[:success] = "Progress logged"
     redirect_to progress.book
   end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,5 @@
 class Note < ActiveRecord::Base
   belongs_to :reading_status
 
-  validates :text, :reading_status presence: true
+  validates :text, :reading_status, presence: true
 end
