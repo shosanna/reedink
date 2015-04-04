e1931e575d9117969d1b927a5f05aa50f32348f5
Jakub Arnold <darthdeus@gmail.com>
2015-04-04 23:28:32 +0200

    Update last_progress_at when starting to read a book

--- app/controllers/reading_statuses_controller.rb
+++ app/controllers/reading_statuses_controller.rb
@@ -1,7 +1,7 @@
 class ReadingStatusesController < AuthenticatedController
   def create
     book = Book.find(params[:book_id])
-    current_user.reading_statuses.create!(book: book)
+    current_user.reading_statuses.create!(book: book, last_progress_at: Time.zone.now)
 
     flash[:success] = "You are now reading #{book.title}."
     redirect_to book
