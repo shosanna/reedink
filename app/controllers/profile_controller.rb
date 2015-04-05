acc9c98fd8b068f32dff380f4431f256c32ec587
Zuzana Dostalova <dostalovaz@gmail.com>
2015-04-05 14:15:41 +0200

    Profile tweaks

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,8 @@
 class ProfileController < AuthenticatedController
   def index
     @books = current_user.books
+    @books_pages = @books.map { |book| [book, book.reading_statuses.first.furthest_read ]}
+
     @skills = current_user.skills
 
     @dataset = 7.times.map { |i| current_user.total_pages_per_day(i.days.ago) }.reverse
