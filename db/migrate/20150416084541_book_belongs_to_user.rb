981c8ef7a90a6b81ddb8e574637b8fe1f45ba6e4
Jakub Arnold <darthdeus@gmail.com>
2015-04-16 15:07:26 +0200

    Fix CI build

--- db/migrate/20150416084541_book_belongs_to_user.rb
+++ db/migrate/20150416084541_book_belongs_to_user.rb
@@ -2,8 +2,10 @@ class BookBelongsToUser < ActiveRecord::Migration
   def change
     add_column :books, :user_id, :uuid
 
-    admin = User.where(email: "darthdeus@gmail.com").first || User.first
+    if Book.count > 0
+      admin = User.where(email: "darthdeus@gmail.com").first || User.first
 
-    Book.update_all(user_id: admin.id)
+      Book.update_all(user_id: admin.id)
+    end
   end
 end
